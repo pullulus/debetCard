4dc633f89cc1312486656f480579b5afa7db68a0
Elena Tsypileva <etsypileva@gmail.com>
2020-10-30 11:02:33 +0300

    changed ApplicationTest.java

--- src/test/java/ru/netology/web/ApplicationTest.java
+++ src/test/java/ru/netology/web/ApplicationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,7 +23,7 @@ class ApplicationTest {
     @BeforeEach
     void setUp() {
 
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
     }
 
     @AfterEach
